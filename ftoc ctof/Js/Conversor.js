a812ff0c071440404ad36e7a0f9709576a6d40ae
Ulises <ulises9alvarenga@gmail.com>
2021-07-31 19:47:52 -0300

    Ejercicios de conversion de temperatura genial

---
+++ ftoc ctof/Js/Conversor.js
@@ -0,0 +1,11 @@
+function FarenheitToCelcius(GradoFarenheit) {
+	const resultado = (GradoFarenheit - 32) * 5 / 9; //( x°F − 32) × 5/9 = 0 °C
+	console.log(resultado + "°C");
+}
+function CelsiusToFarenheit(GradoCelsius) {
+	const resultado = (GradoCelsius * 9 / 5) + 32; //(0 °C × 9/5) + 32 = 32 °F
+	console.log(resultado + "°C");
+}
+
+
+
